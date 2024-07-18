6e46f256cf100e80c288e32bcc72d701954c6069
MoustacheTheCat <113124887+MoustacheTheCat@users.noreply.github.com>
2024-07-18 16:27:51 +0200

    crud receipes and user

--- var/cache/dev/url_matching_routes.php
+++ var/cache/dev/url_matching_routes.php
@@ -15,12 +15,17 @@ return [
         '/_profiler/xdebug' => [[['_route' => '_profiler_xdebug', '_controller' => 'web_profiler.controller.profiler::xdebugAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
         '/' => [[['_route' => 'app_home', '_controller' => 'App\\Controller\\Public\\HomeController::index'], null, null, null, false, false, null]],
+        '/recipes' => [[['_route' => 'app_recipes_index', '_controller' => 'App\\Controller\\RecipesController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/recipes/new' => [[['_route' => 'app_recipes_new', '_controller' => 'App\\Controller\\RecipesController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
         '/register' => [[['_route' => 'app_register', '_controller' => 'App\\Controller\\RegistrationController::register'], null, null, null, false, false, null]],
         '/verify/email' => [[['_route' => 'app_verify_email', '_controller' => 'App\\Controller\\RegistrationController::verifyUserEmail'], null, null, null, false, false, null]],
         '/reset-password' => [[['_route' => 'app_forgot_password_request', '_controller' => 'App\\Controller\\ResetPasswordController::request'], null, null, null, false, false, null]],
         '/reset-password/check-email' => [[['_route' => 'app_check_email', '_controller' => 'App\\Controller\\ResetPasswordController::checkEmail'], null, null, null, false, false, null]],
         '/login' => [[['_route' => 'app_login', '_controller' => 'App\\Controller\\SecurityController::login'], null, null, null, false, false, null]],
         '/logout' => [[['_route' => 'app_logout', '_controller' => 'App\\Controller\\SecurityController::logout'], null, null, null, false, false, null]],
+        '/user' => [[['_route' => 'app_user_index', '_controller' => 'App\\Controller\\UserController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/user/new' => [[['_route' => 'app_user_new', '_controller' => 'App\\Controller\\UserController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/user/profil' => [[['_route' => 'app_user_show', '_controller' => 'App\\Controller\\UserController::show'], null, ['GET' => 0], null, false, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
@@ -42,7 +47,18 @@ return [
                         .')'
                     .')'
                 .')'
-                .'|/reset\\-password/reset(?:/([^/]++))?(*:238)'
+                .'|/re(?'
+                    .'|cipes/([^/]++)(?'
+                        .'|(*:225)'
+                        .'|/edit(*:238)'
+                        .'|(*:246)'
+                    .')'
+                    .'|set\\-password/reset(?:/([^/]++))?(*:288)'
+                .')'
+                .'|/user/([^/]++)(?'
+                    .'|/edit(*:319)'
+                    .'|(*:327)'
+                .')'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
@@ -54,8 +70,13 @@ return [
         168 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
         181 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
         191 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
-        238 => [
-            [['_route' => 'app_reset_password', 'token' => null, '_controller' => 'App\\Controller\\ResetPasswordController::reset'], ['token'], null, null, false, true, null],
+        225 => [[['_route' => 'app_recipes_show', '_controller' => 'App\\Controller\\RecipesController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        238 => [[['_route' => 'app_recipes_edit', '_controller' => 'App\\Controller\\RecipesController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        246 => [[['_route' => 'app_recipes_delete', '_controller' => 'App\\Controller\\RecipesController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        288 => [[['_route' => 'app_reset_password', 'token' => null, '_controller' => 'App\\Controller\\ResetPasswordController::reset'], ['token'], null, null, false, true, null]],
+        319 => [[['_route' => 'app_user_edit', '_controller' => 'App\\Controller\\UserController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        327 => [
+            [['_route' => 'app_user_delete', '_controller' => 'App\\Controller\\UserController::delete'], ['id'], ['POST' => 0], null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
