dff31290809a7f79d99b110c34ba2029dd753c2b
MoustacheTheCat <113124887+MoustacheTheCat@users.noreply.github.com>
2024-07-21 18:59:56 +0200

    add redirection access denied

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Twig\Environment;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        // ... vous pouvez ajouter votre logique ici
+        return new Response($this->twig->render('access_denied.html.twig'));
+    }
+}
